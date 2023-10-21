1d83984e7c03f93d454be737ade9d1c8c1fde8ae
Herlyn David <herlindavid269@gmail.com>
2023-10-21 16:27:06 -0500

    Se hicieron las validaciones del mododulo de clientes

--- src/components/cliente/AgregarCliente.jsx
+++ src/components/cliente/AgregarCliente.jsx
@@ -24,9 +24,74 @@ const AgregarCliente = () => {
         setCliente(prev => ({ ...prev, [e.target.name]: e.target.value }))
     }
 
+
+        //PARA LAS VALIDACIONES
+    //set le pasa el mensaje de validacion a Error
+    const [nombreError, setNombreError] = useState("");
+    const [apellidoError, setApellidoError] = useState("");
+    const [cedulaError, setCedulaError] = useState("");
+    const [telefonoError, setTelefonoError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [direccionError, setDireccionError] = useState("");
+
     const handleClick = async e => {
 
         e.preventDefault()
+
+         // Validación de nombre para que contenga al menos una letra y espacios en blanco
+        const nameRegex = /[A-Za-z]+/;
+
+        if (!cliente.nombre.match(nameRegex)) {
+            setNombreError("El Nombre debe contener solo letras");
+            return;
+        } else {
+            setNombreError("");
+        }
+
+        // Validación del apellido para que solo contenga letras
+        if (!cliente.apellido.match(nameRegex)) {
+            setApellidoError("El Apellido debe contener solo letras");
+            return;
+        }
+
+        // Validación de cédula para que sea un número y tenga una longitud entre 7 y 11 caracteres
+        const cedulaRegex = /^\d+$/;
+
+        if (!cliente.cedula.match(cedulaRegex) || cliente.cedula.length < 7 || cliente.cedula.length > 11) {
+            setCedulaError("La Cédula debe contener solo números y tener entre 7 y 11 caracteres");
+            return;
+        } else {
+            setCedulaError("");
+        }
+
+        // Validación de teléfono para que contenga solo números y esté entre 7 y 11 dígitos
+        const telefonoRegex = /^\d+$/;
+
+        if (!cliente.telefono.match(telefonoRegex) || cliente.telefono.length < 7 || cliente.telefono.length > 11) {
+            setTelefonoError("El Teléfono debe contener solo números y tener entre 7 y 11 dígitos");
+            return;
+        } else {
+            setTelefonoError("");
+        }
+
+         // Validación del formato del correo electrónico
+         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+         if (!cliente.email.match(emailRegex)) {
+             setEmailError("El Email no tiene un formato válido");
+             return;
+         } else {
+             setEmailError("");
+         }
+
+         // Validación de dirección para que no esté vacía y tenga un mínimo de 5 caracteres
+        if (cliente.direccion.trim() === "" || cliente.direccion.length < 5) {
+            setDireccionError("La Dirección es obligatoria y debe tener al menos 5 caracteres");
+            return;
+        } else {
+            setDireccionError("");
+        }
+
         try {
             await axios.post("http://localhost:3000/api/clientes", cliente)
             navigate()
@@ -34,7 +99,7 @@ const AgregarCliente = () => {
             const modalInstance = bootstrap.Modal.getInstance(modal);
             modalInstance.hide();
 
-            location.reload();
+            Window.location.reload();
 
         } catch (err) {
             console.log(err)
@@ -59,26 +124,44 @@ const AgregarCliente = () => {
                                                 <label for="nombreGuardar" class="col-form-label">Nombre:*</label>
                                                 <input type="text" class="form-control" name="nombre" placeholder=". . ." onChange={handleChange}/>
                                             </div>
+                                                
+                                            {/* en esta etiqueta va salir el error de validacion  */}
+                                            <p className="text-red-500" style={{fontSize:14}}>{nombreError}</p>
+
                                             <div class="mb-3" name="divApellido">
                                                 <label for="apellidoGuardar" class="col-form-label">Apellido:*</label>
                                                 <input type="text" class="form-control" name="apellido" placeholder=". . ." onChange={handleChange}/>
                                             </div>
+                                              {/* en esta etiqueta va salir el error de validacion  */}
+                                              <p className="text-red-500" style={{fontSize:14}}>{apellidoError}</p>
+                                            
                                             <div class="mb-3" name="divCedula">
                                                 <label for="cedulaGuardar" class="col-form-label">Cedula:*</label>
                                                 <input type="text" class="form-control" name="cedula" placeholder=". . ." onChange={handleChange}/>
                                             </div>
+                                              {/* en esta etiqueta va salir el error de validacion  */}
+                                              <p className="text-red-500" style={{fontSize:14}}>{cedulaError}</p>
+                                            
                                             <div class="mb-3" name="divTelefono">
                                                 <label for="telefonoGuardar" class="col-form-label">Teléfono:*</label>
                                                 <input type="text" class="form-control" name="telefono" placeholder=". . ." onChange={handleChange}/>
                                             </div>
+                                              {/* en esta etiqueta va salir el error de validacion  */}
+                                              <p className="text-red-500" style={{fontSize:14}}>{telefonoError}</p>
+                                            
                                             <div class="mb-3" name="divEmail">
                                                 <label for="emailGuardar" class="col-form-label">Email:*</label>
                                                 <input type="email" class="form-control" name="email" placeholder=". . ." onChange={handleChange}/>
                                             </div>
+                                              {/* en esta etiqueta va salir el error de validacion  */}
+                                              <p className="text-red-500" style={{fontSize:14}}>{emailError}</p>
+                                           
                                             <div class="mb-3" name="divDireccion">
                                                 <label for="direccionGuardar" class="col-form-label">Dirección:*</label>
                                                 <input type="text" class="form-control" name="direccion" placeholder=". . ." onChange={handleChange}/>
                                             </div>
+                                              {/* en esta etiqueta va salir el error de validacion  */}
+                                              <p className="text-red-500" style={{fontSize:14}}>{direccionError}</p>
                                         
                                             <div class="modal-footer">
                                                 <button type="button" id="guardarCancelado" class="btn-c" data-bs-dismiss="modal">Cancelar</button>
