004abe359f13d7e4f652dc9931b792214f52ec53
Jesús Cochero <jesus3928cf@gmail.com>
2023-10-25 23:10:57 -0500

    Tarea Listar diseños finalizada

--- src/components/chared/BotonCambioEstado.jsx
+++ src/components/chared/BotonCambioEstado.jsx
@@ -1,11 +1,11 @@
 import './BotonCambioEstado.css'
 import PropTypes from 'prop-types'
 
-const BotonCambioEstado = ({id}) => {
+const BotonCambioEstado = ({ id, isChecked }) => {
     return (
         <label className='switch-button' htmlFor={id}>
             <div className='switch-outer'>
-                <input id={id} type='checkbox' />
+                <input id={id} type='checkbox' checked={isChecked} />
                 <div className='button'>
                     <span className='button-toggle'></span>
                     <span className='button-indicator'></span>
@@ -18,6 +18,7 @@ const BotonCambioEstado = ({id}) => {
 //* Definimos los propTypes de este componente
 BotonCambioEstado.propTypes = {
     id: PropTypes.number.isRequired,
+    isChecked: PropTypes.bool.isRequired
 }
 
 export default BotonCambioEstado;
