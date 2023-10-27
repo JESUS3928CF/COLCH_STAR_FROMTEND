7687e9736c05e43454f576879b3caee76a25a0eb
Brian-3112 <brian3112@gmail.com>
2023-10-27 02:45:15 -0500

    buscador listo

--- src/components/proveedor/ListarProveedores.jsx
+++ src/components/proveedor/ListarProveedores.jsx
@@ -6,14 +6,19 @@ import '../../css-general/tailwind.min.css'
 import '../../css-general/inicio_style.css'
 import '../../css-general/table.min.css'
 import BotonCambioEstado from '../chared/BotonCambioEstado';
-import Buscador from '../chared/Buscador'
+// import Buscador from '../chared/Buscador'
 import EditarProveedor from './EditarProveedor';
 
 
 const ListarProveedores = () => {
 
+    //estado de la baara buscador
+    const [Buscar, setBuscar] = useState("");
+
+
     // conexión para traer todos los datos de la base de datos, con proveedor es que s eva acer el mapeo en la tabla listar
     const [proveedores, setProveedor] = useState([]);
+
 
     // solicitud  a la url
     useEffect(() => {
@@ -25,7 +30,7 @@ const ListarProveedores = () => {
             .catch(error => {
                 console.error('Error al obtener la lista de proveedores', error);
             })
-    }, [ ]);
+    }, []);
 
 
 
@@ -42,6 +47,7 @@ const ListarProveedores = () => {
     const contentStyle = {
         marginLeft: '260px', // Ancho del Navbar
     };
+
 
 
 
@@ -63,7 +69,19 @@ const ListarProveedores = () => {
 
                     {/* botón de buscar */}
                     <div className={styles.buscador}>
-                        <Buscador />
+                        <form className='d-flex'>
+                            <input
+                                id='barra-buscar'
+                                className='form-control me-2'
+                                type='search'
+                                placeholder='Buscar...'
+                                aria-label='Search'
+                                onChange={(e) => setBuscar(e.target.value)}
+                            />
+                            {/* <button id="btn-buscar" className="btn btn-outline-success" type="submit">Buscar</button> */}
+
+
+                        </form>
 
                     </div>
                 </div>
@@ -86,8 +104,20 @@ const ListarProveedores = () => {
                         </thead>
                         <tbody>
 
-                            {/* con los datos traidos por set proveedor se hace un mapeo */}
-                            {proveedores.map(proveedor => (
+                            {/* //filtra los datos que hay en proveedores, y hace la busqueda por cualquier campo */}
+                            {proveedores.filter((campo => {
+                                //aca dice que el termino de buesqueda es lo que se le increce al input es decir Buscar
+                                const terminoBusqueda = Buscar.toLowerCase();
+                                return (
+                                    // aca se le dice a nombre que si encuntra lo que tiene terminoBusqueda que es 
+                                    // que se le incresa al input
+                                    campo.nombre.toLowerCase().includes(terminoBusqueda) ||
+                                    campo.telefono.toLowerCase().includes(terminoBusqueda) ||
+                                    campo.direccion.toLowerCase().includes(terminoBusqueda) ||
+                                    campo.identificador.toLowerCase().includes(terminoBusqueda)
+                                );
+                                {/* con los datos traidos por set proveedor se hace un mapeo */ }
+                            })).map(proveedor => (
                                 <tr key={proveedor.id}>
                                     <td>{proveedor.id_proveedor}</td>
                                     <td>{proveedor.nombre}</td>
@@ -105,7 +135,7 @@ const ListarProveedores = () => {
                         </tbody>
                     </table>
                 </div>
-                <EditarProveedor editarProveedor= {editarProveedor} />
+                <EditarProveedor editarProveedor={editarProveedor} />
             </div>
         </div>
 
