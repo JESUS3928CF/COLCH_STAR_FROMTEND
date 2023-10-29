cd7df952c19795e07bb5fcd3b2a5540c0185dc3b
Jesús Cochero <jesus3928cf@gmail.com>
2023-10-28 22:34:36 -0500

    editar y cambiar publicación de diseño finalizado

--- src/components/diseños/EditarDiseno.jsx
+++ src/components/diseños/EditarDiseno.jsx
@@ -3,8 +3,69 @@ import PropTypes from 'prop-types';
 import CancelarModal from '../chared/CancelarModal';
 import GuardarModal from '../chared/GuardarModal';
 import HeaderModals from '../chared/HeaderModals';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { validarEspaciosVacios, validarImagen } from '../../Validations/validations';
+import AlertaError from '../chared/AlertaError';
+import clienteAxios from '../../config/axios';
+import Swal from 'sweetalert2';
 
 const EditarDiseno = ({detalleDiseno}) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+    } = useForm();
+
+    // Cuando recibe el detalleDiseno, actualiza los valores del formulario
+    useEffect(() => {
+        if (detalleDiseno) {
+            setValue('nombre', detalleDiseno.nombre);
+            setValue('publicado', detalleDiseno.publicado);
+            // Añade las demás propiedades aquí
+        }
+    }, [detalleDiseno]);
+
+
+    const editarDiseno = handleSubmit(async (data) => {
+        console.log(data);
+
+        /// Crear un form-data por que así el back puede recibir imágenes
+        const formData = new FormData();
+        formData.append('nombre', data.nombre.trim());
+        formData.append('publicado', data.publicado);
+        formData.append('imagen', data.imagen[0]);
+
+        /// editar el diseño 
+        try {
+            const res = await clienteAxios.put(`/disenos/${detalleDiseno.id_diseno}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            console.log(res);
+
+            // Lanzar alerta del producto agregado
+            Swal.fire({
+                title: 'Diseño Editado',
+                text: res.data.message,
+                icon: 'success',
+            }).then(() => {
+                location.reload();
+            });
+        } catch (error) {
+            console.log(error);
+            // Lanzar alerta de error
+            Swal.fire({
+                title: 'Error',
+                text: 'Hubo un error',
+                icon: 'Vuelva a intentarlo',
+            }).then(location.reload());
+        }
+    });
+
     return (
         <div className='modal' id='modalDiseño'>
             <div className='modal-dialog modal-dialog-centered'>
@@ -12,7 +73,11 @@ const EditarDiseno = ({detalleDiseno}) => {
                     <HeaderModals title='Editar Diseño' />
                     <div className='modal-body'>
                         {/* formulario para editar un Diseño */}
-                        <form action='' id='formularioAgregarDiseño'>
+                        <form
+                            action=''
+                            id='formularioAgregarDiseño'
+                            onSubmit={editarDiseno}
+                        >
                             <div className='mb-3'>
                                 <label
                                     htmlFor='nombre'
@@ -21,11 +86,26 @@ const EditarDiseno = ({detalleDiseno}) => {
                                     Nombre:
                                 </label>
                                 <input
-                                    value={detalleDiseno.nombre}
+                                    name='nombre'
                                     type='text'
                                     className='form-control'
                                     placeholder='. . .'
+                                    {...register('nombre', {
+                                        required: {
+                                            value: true,
+                                            message: 'El nombre es obligatorio',
+                                        },
+                                        validate: (value) => {
+                                            return validarEspaciosVacios(value);
+                                        },
+                                    })}
                                 />
+                                {/* en esta etiqueta va salir el error de validación  */}
+                                {errors.nombre && (
+                                    <AlertaError
+                                        message={errors.nombre.message}
+                                    />
+                                )}
                             </div>
                             <div className='mb-3'>
                                 <label htmlFor='imagen' className='form-label'>
@@ -35,7 +115,18 @@ const EditarDiseno = ({detalleDiseno}) => {
                                     className='form-control'
                                     name='imagen'
                                     type='file'
+                                    {...register('imagen', {
+                                        validate: (value) => {
+                                            return validarImagen(value[0]);
+                                        },
+                                    })}
                                 />
+                                {/* en esta etiqueta va salir el error de validación  */}
+                                {errors.imagen && (
+                                    <AlertaError
+                                        message={errors.imagen.message}
+                                    />
+                                )}
                             </div>
 
                             <div className='mb-3'>
@@ -47,11 +138,27 @@ const EditarDiseno = ({detalleDiseno}) => {
                                 </label>
                                 <select
                                     className='form-control'
-                                    name='selectRol'
+                                    name='publicado'
+                                    {...register('publicado', {
+                                        required: {
+                                            value: true,
+                                            message:
+                                                'El estado de publicación es obligatorio',
+                                        },
+                                    })}
                                 >
-                                    <option value='true' selected={detalleDiseno.publicado === true}>Si</option>
-                                    <option value='false' selected={detalleDiseno.publicado === false}>No</option>
+                                    <option value='' disabled selected>
+                                        Selecciona una opción
+                                    </option>
+                                    <option value='true'>Si</option>
+                                    <option value='false'>No</option>
                                 </select>
+                                {/* en esta etiqueta va salir el error de validación  */}
+                                {errors.publicado && (
+                                    <AlertaError
+                                        message={errors.publicado.message}
+                                    />
+                                )}
                             </div>
 
                             <div className='modal-footer'>
