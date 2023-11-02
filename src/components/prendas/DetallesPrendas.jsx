e9f6d53875892ee296f4d969ae4d728aff3b6be3
Ts2533 <tomasvanegas2533@gmail.com>
2023-11-01 20:21:41 -0500

    prendas

--- src/components/prendas/DetallesPrendas.jsx
+++ src/components/prendas/DetallesPrendas.jsx
@@ -1,8 +1,19 @@
 import PropTypes from 'prop-types';
 
 import HeaderModals from '../chared/HeaderModals';
+// import { useForm } from 'react-hook-form';
+// import { useEffect } from 'react';
 
 export const DetallesPrendas = ({ detallesPrendas }) => {
+    // const {setValue}=useForm
+
+    // useEffect(()=>{
+    //     if(detallesPrendas){
+    //         setValue('publicado', detallesPrendas.publicado)
+    //     }
+    // },[detallesPrendas])
+
+    
     return (
         <div className='modal' id='modalDetallePrendas'>
             <div className='modal-dialog modal-dialog-centered'>
@@ -13,26 +24,27 @@ export const DetallesPrendas = ({ detallesPrendas }) => {
                             <div className='container'>
                                 <div className='col'>
                                     <div className='row'>
-                                        <div>
-                                            <div className='mb-3'>
-                                            <h2 htmlFor="Tela" className="fs-4  text-nowrap bg-body-secondary border"  > Tipo de tela: {detallesPrendas.tipo_de_tela}</h2>
-                                            </div>
-
-                                            <div className='mb-3'>
-                                                <label htmlFor="Genero" className="col-form-label"> Genero: {detallesPrendas.genero} </label>
-                                            </div>
-
-                                            <div className='container ml-12'>
-                                                <img   src={`${
+                                        <div className='row gx-0'>
+                                            <div className='col-md-4'>
+                                            <img   src={`${
                                                     import.meta.env
                                                         .VITE_BACKEND_URL
                                                 }/${detallesPrendas.imagen}`}
                                                 width='250px'
                                                 height='200px'
-                                                alt=''/>
+                                                alt=''/>                                   
                                             </div>
+                                            <div className='col-md-8'>
+                                                <div className='card-body'>
+                                                    <h1 htmlFor="nombre" className='card-title'>Nombre: {detallesPrendas.nombre}</h1>
+                                                    <h1 htmlFor="cantidad" className='card-title'>Cantidad: {detallesPrendas.cantidad}</h1>
+                                                    <h1 htmlFor="precio" className='card-title'>Precio: {detallesPrendas.precio}</h1>
+                                                    <h1 htmlFor="Tela" className="card-title"> Tipo de tela: {detallesPrendas.tipo_de_tela}</h1>
+                                                    <h1 htmlFor="genero" className="card-title"> Genero: {detallesPrendas.genero}</h1>                                        
+                                                    <h1 htmlFor="Publicado" name='publicado' className="card-title" > Publicado: {detallesPrendas.publicado}</h1>                                        
+                                                </div>
 
-
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
