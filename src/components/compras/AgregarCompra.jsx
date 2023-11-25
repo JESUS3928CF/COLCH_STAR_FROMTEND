49355cda33197a088ad220dd272392a38cd469ad
Ts2533 <tomasvanegas2533@gmail.com>
2023-11-25 18:13:51 -0500

    Compras

--- src/components/compras/AgregarCompra.jsx
+++ src/components/compras/AgregarCompra.jsx
@@ -6,58 +6,149 @@ import React from "react";
 import CancelarModal from "../chared/CancelarModal";
 import GuardarModal from "../chared/GuardarModal";
 import HeaderModals from "../chared/HeaderModals";
-
-
+import BotonNegro from "../chared/BotonNegro";
 
 const AgregarCompra = () => {
-    return ( 
-        <div>
-            <div className="modal" id="myModal">
-                <div className="modal-dialog modal-dialog-centered">
-                    <div className="modal-content">
-                         <HeaderModals title={"Agregar Compra"} />
-                        <div>
-                            <div className="modal-body ">
-                                <form action="" id="formularioagregarCompra">
-                                    <div className="form-label">
-                                        <p className="detalleCompra">Agregar datos de los productos de la compra</p>
-                                    </div>
-                                    <div className="subFormulario">
-                            
-                                        <div className="mb-3">
-                                            <label htmlFor="nombreCompraAgregar" className="col-form-label">Nombre:</label>
-                                            <input type="text" className="form-control" id="nombreCompraAgregar" name="nombreCompraAgregar" placeholder="Ingresar nombre"/>
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="cantidadCompraAgregar" className="col-form-label">Cantidad:</label>
-                                            <input type="text" className="form-control" id="cantidadCompraAgregar" name="cantidadCompraAgregar" placeholder="Ingresar cantidad"/>
+  return (
+    <>
+      <div>
+        <div className="modal" id="myModal">
+          <div className="modal-dialog modal-dialog-centered ">
+            <div className="modal-content">
+              <HeaderModals title={"Agregar Compra"} />
+              <div>
+                <div className="modal-body ">
+                  <form
+                    action=""
+                    id="formularioagregarCompra"
+                    className="row g-3 needs-validation"
+                  >
+                    <div className="col-md-6">
+                      <label
+                        htmlFor="nombreCompraAgregar"
+                        className="col-form-label"
+                      >
+                        Nombre del proveedor
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="nombreCompraAgregar"
+                        name="nombreCompraAgregar"
+                        placeholder=". . ."
+                      />
+                    </div>
 
-                                        </div>
-                                    </div>
-                                    <div className="subFormulario">
-                                        <div className="mb-3">
-                                            <label htmlFor="precioCompraAgregar" className="col-form-label">Precio:</label>
-                                            <input type="text" className="form-control" id="precioCompraAgregar" name="precioCompraAgregar"
-                                                placeholder="Ingresar precio unitario del producto" />
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="totalCompraAgregar" className="col-form-label">Precio total:</label>
-                                            <input type="text" className="form-control" id="totalCompraAgregar" placeholder="" value="100.000"/>
-                                        </div>
-                                    </div>
-                                    <div className="modal-footer">
-                    <CancelarModal modalToCancel="myModal" />
-                    <GuardarModal />
-                  </div>
+                    <div className="col-md-6">
+                      <label
+                        htmlFor="totalCompraAgregar"
+                        className="col-form-label"
+                      >
+                        Fecha
+                      </label>
+                      <input
+                        type="Date"
+                        className="form-control"
+                        id="totalCompraAgregar"
+                      />
+                    </div>
 
-                                </form>
-                            </div>
-                        </div>
-                       </div>
+                    <div className="col-md-6">
+                        <label
+                          htmlFor="nombreCompraAgregar"
+                          className="col-form-label"
+                        >
+                          Cantidad
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="nombreCompraAgregar"
+                          name="nombreCompraAgregar"
+                          placeholder=". . ."
+                        />
+                      </div>
+
+
+                   
+
+                  
+                      
+
+                      <div className="col-md-6">
+                        <label
+                          htmlFor="totalCompraAgregar"
+                          className="col-form-label"
+                        >
+                          Precio
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="totalCompraAgregar"
+                          placeholder=". . ."
+                        />
+                      </div>
+
+                      <div className="col-md-6">
+                        <label
+                          htmlFor="totalCompraAgregar"
+                          className="col-form-label"
+                        >
+                          Precio Total
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="totalCompraAgregar"
+                          placeholder=". . ."
+                        />
+                      </div>
+
+                     
+                      <div className="col-md-6">
+                        <label
+                          htmlFor="totalCompraAgregar"
+                          className="col-form-label"
+                        >
+                          Diseño
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="totalCompraAgregar"
+                          placeholder=". . ."
+                        />
+                      </div>
+
+                      <div className="mb-3">
+                        <label
+                          htmlFor="totalCompraAgregar"
+                          className="col-form-label"
+                        >
+                          Prendas
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          id="totalCompraAgregar"
+                          placeholder=". . ."
+                        />
+                      </div>
+                    </form>
+
+                    <div className="modal-footer">
+                      <CancelarModal modalToCancel="myModal" />
+                      <GuardarModal />
                     </div>
                 </div>
+              </div>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default AgregarCompra;
