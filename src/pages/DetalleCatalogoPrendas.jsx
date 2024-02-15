a2dd882d64e3278fd0ec49f264008ea71f674a76
Brian Pareja <parejabrianh12@gmail.com>
2024-02-15 16:25:23 -0500

    OEE

--- src/pages/DetalleCatalogoPrendas.jsx
+++ src/pages/DetalleCatalogoPrendas.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types';
 import HeaderModals from '../components/chared/HeaderModals';
-import { FcApproval, FcCancel } from 'react-icons/fc';
-import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
 
 
@@ -29,10 +28,86 @@ const DetalleCatalogoPrendas = ({ DetallesPrendasCatalogo, show, handleClose, ha
                             // setSelectedDisenoNombre([])
                         }}
                     />
+                    <div className="formulario">
+                        <div className="modal-body">
+                            <div className="container">
+                                <div className="col">
+                                    <form className="row g-3 needs-validation">
+                                        <div className="mb-2" name="divIdentificacion">
+                                            <label
+                                                htmlFor="identificacionEditar"
+                                                className="col-form-label"
+                                            >
+                                                PRECIO:
+                                            </label>
+                                            <br />
 
-                    <div className='modal-body'>
-                        
-                    
+                                            <div>
+
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    value={DetallesPrendasCatalogo.precio}
+
+                                                    readOnly
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <label htmlFor="nombre" className="col-form-label">
+                                                Nombres:
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                readOnly
+                                            />
+                                        </div>
+                                        <div className="col-md-6">
+                                            <label htmlFor="apellido" className="col-form-label">
+                                                Apellidos:
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                readOnly
+                                            />
+                                        </div>
+                                        <div className="col-md-6">
+                                            <label htmlFor="telefono" className="col-form-label">
+                                                Teléfono:
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                readOnly
+                                            />
+                                        </div>
+                                        <div className="col-md-6">
+                                            <label htmlFor="direccion" className="col-form-label">
+                                                Dirección:
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                readOnly
+                                            />
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="email" className="col-form-label">
+                                                Email:
+                                            </label>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+
+                                                readOnly
+                                            />
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Modal>
